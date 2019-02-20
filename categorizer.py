105a8ff2ffb4b8b53d3442c3737d236cd364a0fd
alexewu <alexewu@umich.edu>
2019-02-19 23:01:30 -0500

    created folders and gonna delete old categorizer

--- categorizer.py
+++ categorizer.py
@@ -1,31 +1,24 @@
 from __future__ import unicode_literals
-from spacy.vectors import Vectors
+#from spacy.vectors import Vectors
 import spacy
 
 #nlp = spacy.load('en')
 nlp = spacy.load('en_vectors_web_lg')
 
 
-tokens = nlp(u'dog cat banana')
+tokens = nlp(u'RT @SOTTnet: Earth Changes: Aerial footage of devastating Colorado floods: Aerial footage shows the scale of destruction ca... http://t.co/\u2026')
 
 data = []
 for token1 in tokens:
     row = []
+    value = 0 #the sum of similarities with other words in the bag
     row.append(token1.text)
     for token2 in tokens:
-        row.append(token1.similarity(token2))
+        value += token1.similarity(token2)
+    row.append(value)
     data.append(row)
-        #print(token1.text, token2.text, token1.similarity(token2))
 
-# empty_vectors = Vectors(shape=(10000, 300))
-#
-# data = numpy.zeros((3, 300), dtype='f')
-# keys = [u'cat', u'dog', u'rat']
-# vectors = Vectors(data=data, keys=keys)
-# for row in data:
-#     for val in row:
-#         print '{:4}'.format(val),
-#     print
-
-print('\n'.join([''.join(['{:4}'.format(item) for item in row])
-      for row in data]))
+text_file = open("post.txt", "w")
+for row in data:
+    text_file.write("%s %f\n" % (row[0], row[1]))
+text_file.close()
