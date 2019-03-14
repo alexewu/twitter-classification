095e5d23154110f19d0a771ba477cb67ae89ac65
alexewu <alexewu@umich.edu>
2019-03-14 16:26:49 -0400

    Changed script to include the percent accuracy
    trains on 80% instead of 10%
    tested on the 10%-developing
    return the percent accuracy and outputs sent to ratings.json

--- scripts/ex_cat.py
+++ scripts/ex_cat.py
@@ -2,20 +2,6 @@ import spacy
 import json
 nlp = spacy.load('en')
 
-# train_data = [
-#     (u"[ #COSTARICA #TSUNAMI ADVISORY ]: NO TSUNAMI WAVES REPORTED YET--BE ON ALERT! PRECAUTIONARY EVACUATION RECOMMENDED! #Earthquake #PTWC",
-#      {"cats": {"LOW": 0, "MEDIUM": 0, "HIGH": 1}}),
-#     (u"Sott - Costa Rican Officials: A Strong Earthquake Could Still Occur in Guanacaste http://t.co/Y6s3nUc9", {"cats": {"LOW": 0, "MEDIUM": 1, "HIGH": 0}}),
-#     (u"Bummer for Caribbean locals &amp; vacationers. Major quake hits near Costa Rica coast: http://t.co/6sL4AtOj Hope all is well down there!", {"cats": {"LOW": 1, "MEDIUM": 0, "HIGH": 0}}),
-#     (u"@TheNextCorner you missed our earthquake 7.6 in Costa Rica", {"cats": {"LOW": 1, "MEDIUM": 0, "HIGH": 0}}),
-#     (u"Two dead, 20 injured as major earthquake  at magnitude 7.6 damages Costa Rica coast, Red Cross says.", {"cats": {"LOW": 0, "MEDIUM": 0, "HIGH": 1}}),
-#     (u"The High Park fire west of Fort Collins, #CO has consumed 36,930 acres so far, is 0% contained and continues to grow. #NWS #cowx #cofire", {"cats": {"LOW": 1, "MEDIUM": 0, "HIGH": 0}}),
-#     (u"RT @ColoradoRapids: Photo of #FlagStaffFire in Boulder as seen from @DSGPark at 4pm MT. So many wildfires throughout Colorado rt now,  h ...", {"cats": {"LOW": 0, "MEDIUM": 1, "HIGH": 0}}),
-#     (u"RT @LarimerSheriff: #HighParkFire Another evacuation area notice has been lifted. Please check out link http://t.co/KXPBivpb", {"cats": {"LOW": 0, "MEDIUM": 0, "HIGH": 1}}),
-#     (u"RT @Kswan11: Pic of #highparkfire from Estes park high school http://t.co/hTVMKlk6", {"cats": {"LOW": 0, "MEDIUM": 1, "HIGH": 0}}),
-#     (u"Unprecedented Colorado Wildfires Continue, As Strong Winds and Looting Add to the Problem http://t.co/HwKpHLBt", {"cats": {"LOW": 0, "MEDIUM": 0, "HIGH": 1}}),
-# ]
-
 with open("/Users/alexandrawu/Desktop/twitter2/venv/twitter-classification/all-posts/80.json") as f:
     data = json.load(f)
 
@@ -23,33 +9,62 @@ train_data = []
 for post in data:
     priority = {}
     if post["priority"] == "Low":
-        priority = {"cats": {"LOW": 1, "MEDIUM": 0, "HIGH": 0}}
+        priority = {"cats": {"Low": 1, "Medium": 0, "High": 0}}
     elif post["priority"] == "Medium":
-        priority = {"cats": {"LOW": 0, "MEDIUM": 1, "HIGH": 0}}
+        priority = {"cats": {"Low": 0, "Medium": 1, "High": 0}}
     else:
-        priority = {"cats": {"LOW": 0, "MEDIUM": 0, "HIGH": 1}}
+        priority = {"cats": {"Low": 0, "Medium": 0, "High": 1}}
     pair = (post["content"], priority)
     train_data.append(pair)
 
 
 textcat = nlp.create_pipe('textcat')
 nlp.add_pipe(textcat, last=True)
-textcat.add_label('LOW')
-textcat.add_label('MEDIUM')
-textcat.add_label('HIGH')
+textcat.add_label('Low')
+textcat.add_label('Medium')
+textcat.add_label('High')
 optimizer = nlp.begin_training()
 for itn in range(10):
     for doc, gold in train_data:
         nlp.update([doc], [gold], sgd=optimizer)
 
-#low
-doc = nlp(u"Sott - Costa Rican Officials: A Strong Earthquake Could Still Occur in Guanacaste http://t.co/Y6s3nUc9")
-print(doc.cats)
+# #sanity check with posts in the training set
+# #low
+# doc = nlp(u"Bummer for Caribbean locals &amp; vacationers. Major quake hits near Costa Rica coast: http://t.co/6sL4AtOj Hope all is well down there!")
+# print(doc.cats)
+#
+# #medium
+# doc = nlp(u"Sott - Costa Rican Officials: A Strong Earthquake Could Still Occur in Guanacaste http://t.co/Y6s3nUc9")
+# print(doc.cats)
+#
+# #high
+# doc = nlp(u"@alan_arnette Moore Animal Hosptal in FC offering free small animal boarding and pick up for #highparkfire evacuees. 970-560-0452 for info.")
+# print(doc.cats)
 
-#medium
-doc = nlp(u"RT @ColoradoRapids: Photo of #FlagStaffFire in Boulder as seen from @DSGPark at 4pm MT. So many wildfires throughout Colorado rt now,  h ...")
-print(doc.cats)
+with open("/Users/alexandrawu/Desktop/twitter2/venv/twitter-classification/all-posts/10-dev.json") as f:
+    test_data = json.load(f)
 
-#high
-doc = nlp(u"Unprecedented Colorado Wildfires Continue, As Strong Winds and Looting Add to the Problem http://t.co/HwKpHLBt")
-print(doc.cats)
+num_correct = 0
+num_total = 0
+output = []
+for post in test_data:
+    doc = nlp(post["content"])
+    print(doc.cats)
+    result = doc.cats
+    output.append(result)
+    num_total += 1
+    maxValue = max(result.values())
+    maxPriority = [k for k,v in result.items() if v == maxValue]
+    if maxPriority[0] == post["priority"]:
+        num_correct += 1
+
+with open('/Users/alexandrawu/Desktop/twitter/ratings.json', 'w') as outfile:
+    json.dump(output, outfile, indent=4)
+
+print("The number of correctly categorized posts is %d out of %d", num_correct, num_total)
+print("The percent correctly calculated is %d", num_correct/float(num_total))
+
+
+
+
+
